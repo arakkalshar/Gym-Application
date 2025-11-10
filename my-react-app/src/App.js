92a1714dc7c8c73100f1de1150924e1f8b7a8574
Samhith9981 <samhithgovada9981@outlook.com>
2025-11-09 19:19:07 -0800

    Testing app.js changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi there! Welcome to my React App!
         </a>
       </header>
     </div>
