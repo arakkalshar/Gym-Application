ca6e152d0be0b42c8d8a3d64b689a5f5fae26e1e
storeyr <storeyr@vcu.edu>
2025-11-21 00:56:13 -0500

    ProgressDashboard commit 1

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { App } from './App';
 
-const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+let container = document.getElementById("app");
+let root = createRoot(container)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
